816269a5bffb6c092f8babcf7c9b97b704f77aef
Jaseelshalu <123951774+Jaseelshalu@users.noreply.github.com>
2023-07-21 15:50:30 +0530

    app

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
+
+var hbs = require('express-handlebars') //new
 
 var app = express();
+
+var fileUpload = require('express-fileupload') //new
+var db = require('./config/connection') //new
+var session = require('express-session') //new
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials/' })) //new
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,16 +27,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(fileUpload()) //new
+app.use(session({ secret: "key", cookie: { maxAge: 600000 } }))
+
+db.connect((err) => {
+  if (err) console.log('Database Connection Error: ' + err);
+  else console.log('Database Connected');
+}) //new
+
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
